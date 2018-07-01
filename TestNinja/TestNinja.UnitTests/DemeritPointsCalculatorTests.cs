18b15286a3c990a1126aa68bf70935288a7fee7f
albert18 <caps18@hotmail.com>
2018-07-01 18:29:55 -0400

    External Dependecies

---
+++ TestNinja/TestNinja.UnitTests/DemeritPointsCalculatorTests.cs
@@ -0,0 +1,58 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class DemeritPointsCalculatorTests
+    {
+        //[Test]
+        //public void CalculateDemeritPoints_SpeedIsNegative_ThrowArguments()
+        //{
+        //    var calculator = new DemeritPointsCalculator();
+
+        //    Assert.That(() => calculator.CalculateDemeritPoints(-1), Throws.Exception.TypeOf<ArgumentOutOfRangeException>());
+
+            
+        //}
+
+        //[Test]
+        //public void CalculateDemeritPoints_SpeedIsOver300_ThrowArguments()
+        //{
+        //}
+
+
+
+
+        //[Test]
+        //[TestCase(0, 0)]
+        //[TestCase(64, 0)]
+        //[TestCase(64, 0)]
+        //[TestCase(70, 0)]
+        //[TestCase(75, 0)]
+        //public void DemeritPointsCalculator_Speed(int speed)
+        //{
+        //    //var race = new DemeritPointsCalculator();
+
+        //    //var result = race.CalculateDemeritPoints(75);
+
+        //    //Assert.That(result, Is.EqualTo(2));
+        //}
+
+
+        ////[Test]
+        ////public void DemeritPointsCalculator_Speed()
+        ////{
+        ////    var race = new DemeritPointsCalculator();
+
+        ////    var result = race.CalculateDemeritPoints(75);
+
+        ////    Assert.That(result, Is.EqualTo(2));
+        ////}
+    }
+}
---
+++ TestNinja/TestNinja.UnitTests/FizzBuzzTests.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class FizzBuzzTests
+    {
+
+        public void GetOutput_First()
+        {
+            //Act
+            var result = FizzBuzz.GetOutput(15);
+        
+            //Assert
+            Assert.That(result, Is.EqualTo("FizzBuzz"));
+        }
+
+        public void GetOutput_Second()
+        {
+            //Act
+            var result = FizzBuzz.GetOutput(3);
+
+            //Assert
+            Assert.That(result, Is.EqualTo("Fizz"));
+        }
+
+        public void GetOutput_Third()
+        {
+            //Act
+            var result = FizzBuzz.GetOutput(5);
+
+            //Assert
+            Assert.That(result, Is.EqualTo("Buzz"));
+        }
+
+        public void GetOutput_Last()
+        {
+            //Act
+            var result = FizzBuzz.GetOutput(1);
+
+            //Assert
+            Assert.That(result, Is.EqualTo("1"));
+        }
+    }
+}
